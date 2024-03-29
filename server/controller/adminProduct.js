e9ebf12e3da82cd2ae2b96accd601d210b969fbb
Rohan-chy <chyrohan036@gmail.com>
2024-03-28 19:23:51 -0700

    update and delete product along with image file

--- server/controller/adminProduct.js
+++ server/controller/adminProduct.js
@@ -1,4 +1,5 @@
 const productModel = require("../model/productModel");
+const fs=require('fs')
 
 exports.adminProductController=async(req,res)=>{
 
@@ -97,4 +98,130 @@ exports.singleProduct=async(req,res)=>{
         message:'product fetched success',
         product:singleProduct
     })
+}
+
+
+//delete product
+exports.deleteProduct=async(req,res)=>{
+    const {id}=req.params;
+    if(!id){
+        return res.status(400).json({
+            message:"please provide id"
+        })
+    }
+
+    const oldData=await productModel.findById(id);
+    const oldImage=oldData.productImage;
+    const server_url_length=process.env.SERVER_URL.length;
+
+    const filter_oldImage=oldImage.slice(server_url_length);
+
+    // deleting images from uploads folder
+    fs.unlink(`./uploads/${filter_oldImage}`,(error)=>{
+    if(error){
+        console.log('image file error:',error);
+    }
+    else{
+        console.log("image file deleted")
+    }
+    }
+    )
+
+
+    await productModel.findByIdAndDelete(id)
+    res.status(200).json({
+        message:'product deleted'
+    })
+}
+
+
+//update product
+exports.updateProduct=async(req,res)=>{
+    const {id}=req.params;
+    const {productName,productDescription,productPrice,productQuantity,productStatus}=req.body;
+
+    
+    //productname
+    if(!productName){
+        return res.status(400).json({
+            message:"please provide productName"
+        })
+    }
+
+    //productDescription
+    if(!productDescription){
+        return res.status(400).json({
+            message:"please provide productDescription"
+        })
+    }
+
+    //productPrice
+    if(!productPrice){
+        return res.status(400).json({
+            message:"please provide productPrice"
+        })
+    }
+
+    //productQuantity
+    if(!productQuantity){
+        return res.status(400).json({
+            message:"please provide productQuantity"
+        })
+    }
+
+    //productStatus
+    if(!productStatus){
+        return res.status(400).json({
+            message:"please provide productStatus"
+        })
+    }
+
+     //id
+    if(!id){
+        return res.status(400).json({
+            message:"please provide id"
+        })
+    }
+
+    const oldData=await productModel.findById(id);
+
+     //oldData
+    if(!oldData){
+        return res.status(400).json({
+            message:"no data found"
+        })
+    }
+
+    const oldImage=oldData.productImage;
+    const server_url_length=process.env.SERVER_URL.length;
+
+    const filter_oldImage=oldImage.slice(server_url_length);
+
+    if(req.file && req.file.filename){
+        fs.unlink(`./uploads/${filter_oldImage}`,(err)=>{
+            if(err){
+                console.log("image update error:",err)
+            }
+            else{
+                console.log("image file updated")
+            }
+        })
+    }
+
+    const data=await productModel.findByIdAndUpdate(id,{
+        productName,
+        productDescription,
+        productPrice,
+        productQuantity,
+        productStatus,
+        productImage: req.file && req.file.filename ? `${process.env.SERVER_URL}/${req.file.filename}`:oldImage
+    },{
+        new:true
+    })
+
+    res.status(200).json({
+        message:"product updated",
+        data:data
+    })
+    
 }
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,4 +1,4 @@
-const { adminProductController, getProducts, singleProduct } = require('../controller/adminProduct');
+const { adminProductController, getProducts, singleProduct, deleteProduct, updateProduct } = require('../controller/adminProduct');
 const isAuthenticated = require('../middleware/isAuthenticated');
 const restrictRole = require('../middleware/restrictRole');
 const {multer,storage}=require('../middleware/multer');
@@ -7,11 +7,15 @@ const upload=multer({storage:storage})
 
 const router=require('express').Router()
 
+// avatar vaneko frontend bata pathako fieldname
 router.route('/product')
 .post(isAuthenticated,restrictRole('admin'),upload.single('avatar'),exceptionalHandling(adminProductController))
 .get(exceptionalHandling(getProducts));
 
-router.route('/product/:id').get(exceptionalHandling(singleProduct));
+router.route('/product/:id')
+.get(exceptionalHandling(singleProduct))
+.delete(isAuthenticated,restrictRole('admin'),exceptionalHandling(deleteProduct))
+.patch(isAuthenticated,restrictRole('admin'),upload.single('avatar'),exceptionalHandling(updateProduct))
 
 
 module.exports=router
