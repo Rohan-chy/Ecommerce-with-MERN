f52509412e0cbf97c94fe2572374157c7c266f6e
Rohan-chy <chyrohan036@gmail.com>
2024-05-10 10:44:10 -0700

    added date filtering

--- client/src/pages/myOrders/MyOrders.jsx
+++ client/src/pages/myOrders/MyOrders.jsx
@@ -7,6 +7,7 @@ const MyOrders = () => {
     const {orders}=useSelector((state)=>state.order)
     const [orderStat,setOrderStatus]=useState('all')
     const [search,setSearch]=useState('')
+    const [date,setDate]=useState('')
     console.log(orders)
 
     useEffect(()=>{
@@ -17,6 +18,7 @@ const MyOrders = () => {
     const filteredOrders=orders.filter((order)=>orderStat==='all'|| order.orderStatus===orderStat).filter((order)=>
         order._id.toLowerCase().includes(search.toLocaleLowerCase()) ||
         order.paymentDetails.method.toLowerCase().includes(search.toLowerCase()))
+        .filter((order)=>date===''|| new Date(order.createdAt).toLocaleDateString()===new Date(date).toLocaleDateString())
   return (
 <div className="antialiased font-sans bg-gray-200 pt-5">
     <div className="container mx-auto px-4 sm:px-8">
@@ -55,6 +57,17 @@ const MyOrders = () => {
                     <input placeholder="Search" onChange={(e)=>setSearch(e.target.value)}
                         className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
                 </div>
+                <div className="block relative">
+                    <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                        <svg viewBox="0 0 24 24" className="h-4 w-4 fill-current text-gray-500">
+                            <path
+                                d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                            </path>
+                        </svg>
+                    </span>
+                    <input type='date' onChange={(e)=>setDate(e.target.value)}
+                        className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+                </div>
             </div>
             <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
                 <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
@@ -75,11 +88,11 @@ const MyOrders = () => {
                                 </th>
                                 <th
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                    Payment Method
+                                    Payment Status
                                 </th>
                                 <th
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                    Payment Status
+                                    Ordered At
                                 </th>
                             </tr>
                         </thead>
@@ -109,18 +122,19 @@ const MyOrders = () => {
                                         {order.orderStatus}
                                     </p>
                                 </td>
-                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-left">
-                                    <p className="text-gray-900 whitespace-no-wrap">
-                                        {order.paymentDetails.method}
-                                    </p>
-                                </td>
+                                
                                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-left">
                                     <span
                                         className={`relative inline-block px-3 py-1 font-semibold ${order.paymentDetails.paymentStatus=='paid'?'text-green-900':'text-red-500'} leading-tight`}>
                                         <span aria-hidden
                                             className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                        <span className="relative">{order.paymentDetails.paymentStatus}</span>
+                                        <span className="relative">{order.paymentDetails.paymentStatus}[{order.paymentDetails.method}]</span>
                                     </span>
+                                </td>
+                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-left">
+                                    <p className="text-gray-900 whitespace-no-wrap">
+                                        {new Date(order.createdAt).toLocaleDateString()}
+                                    </p>
                                 </td>
                             </tr>
                                 ))
