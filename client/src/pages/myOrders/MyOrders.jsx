e56ae51bd63919c0013ddcfbd0f3307b8f2aecb5
Rohan-chy <chyrohan036@gmail.com>
2024-05-10 08:43:38 -0700

    added order status filtering

--- client/src/pages/myOrders/MyOrders.jsx
+++ client/src/pages/myOrders/MyOrders.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchOrder } from '../../store/orderSlice';
 
 const MyOrders = () => {
     const dispatch=useDispatch();
     const {orders}=useSelector((state)=>state.order)
-    console.log(orders)
+    const [orderStat,setOrderStatus]=useState('all')
 
     useEffect(()=>{
         dispatch(fetchOrder())
     },[])
+
+    const filteredOrders=orderStat=='all'? orders:orders.filter((order)=>order.orderStatus==orderStat)
   return (
 <div className="antialiased font-sans bg-gray-200 pt-5">
     <div className="container mx-auto px-4 sm:px-8">
@@ -20,25 +22,14 @@ const MyOrders = () => {
             <div className="my-2 flex sm:flex-row flex-col">
                 <div className="flex flex-row mb-1 sm:mb-0">
                     <div className="relative">
-                        <select
-                            className="appearance-none h-full rounded-l border block  w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                            <option>5</option>
-                            <option>10</option>
-                            <option>20</option>
-                        </select>
-                        <div
-                            className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                            </svg>
-                        </div>
-                    </div>
-                    <div className="relative">
-                        <select
-                            className=" h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-                            <option>All</option>
-                            <option>Active</option>
-                            <option>Inactive</option>
+                        <select onChange={(e)=>setOrderStatus(e.target.value)} className=" h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
+                            <option value='all'>all</option>
+                            <option value='pending'>pending</option>
+                            <option value={'on the way'}>on the way</option>
+                            <option value={'delivered'}>delivered</option>
+                            <option value={'on the way'}>on the way</option>
+                            <option value={'preparation'}>preparation</option>
+                            <option value={'cancelled'}>cancelled</option>
                         </select>
                         <div
                             className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -89,7 +80,7 @@ const MyOrders = () => {
                         </thead>
                         <tbody>
                             {
-                                orders && orders.length>0 && orders.map((order)=>(
+                                filteredOrders && filteredOrders.length>0 && filteredOrders.map((order)=>(
                                 <tr key={order._id}>
                                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                     <div className="flex items-center">
